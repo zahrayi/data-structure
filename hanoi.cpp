926e48402a3bb5e64abdd512a803a71d2f84ef32
zahrayi <134525899+zahrayi@users.noreply.github.com>
2023-11-08 18:13:54 +0330

    Update hanoi.cpp
    test-correcting-code

--- hanoi.cpp
+++ hanoi.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 void hanoi(int n, char source, char destination, char auxiliary) {
     if (n == 1) {
-        std::cout << "Move disk 1 from " << source << " to " << destination << endl;
+        cout << "Move disk 1 from " << source << " to " << destination << endl;
         return;
     }
 
     hanoi(n - 1, source, auxiliary, destination);
-    std::cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+    cout << "Move disk " << n << " from " << source << " to " << destination << endl;
     hanoi(n - 1, auxiliary, destination, source);
 }
 
