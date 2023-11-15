c7109d5fe5bb55e7e1d15fbe7259f2fd4f08d971
elinaaslanian <elina.aslanian8@gmail.com>
2023-11-15 23:15:48 +0330

    add array

---
+++ arrayfinal.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class array {
+public:
+    int find(int a[], int n, int key) {
+        for (int i = 0; i < n; i++) {
+            if (a[i]==key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    int insert(int a[],int add,int n,int c)
+    {
+      if(n>=c)
+      {
+        return n;
+    }
+    a[n]=add;
+    
+    return(n+1);
+  }
+
+    int delet(int a[],int n,int del)
+    {
+    	int p;
+    	p=find(a,n,del);
+    	if(p==-1)
+    	{
+    		return n;
+		}
+		
+		for(int i=p;i<=n-2;i++)
+		{
+			a[i]=a[i+1];
+		}
+		return n-1;
+	}
+};
