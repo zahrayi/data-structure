4c2b7b35538ef0541097392141418b448b1a48d9
zahrayi <134525899+zahrayi@users.noreply.github.com>
2023-11-23 18:19:01 +0330

    Update QUEUE(stack).cpp

--- QUEUE(stack).cpp
+++ QUEUE(stack).cpp
@@ -63,18 +63,3 @@ public:
 
     
 };
-
-int main() {
-    Queue q;
-    q.enqueue(10);
-    q.enqueue(20);
-    q.enqueue(30);
-    q.enqueue(40);
-
-    cout << q.peek() << endl;
-
-    q.dequeue();
-    cout << q.peek() << endl;
-
-    return 0;
-}
