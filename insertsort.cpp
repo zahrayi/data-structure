0f71d7e60833e3c4615182eb964cfcf29538653a
elinaaslanian <149394302+elinaaslanian@users.noreply.github.com>
2024-01-04 23:36:41 +0330

    new

---
+++ insertsort.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        
+        
+        while (j >= 0 && arr[j] > key) {
