48cd8822ec0f2cf3b94b9dd220806274448e8047
elinaaslanian <149394302+elinaaslanian@users.noreply.github.com>
2024-01-04 22:34:12 +0330

    change

---
+++ heapmax.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+using namespace std;
+
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]); 
+        heapify(arr, n, largest);
+    }
+}
+
+
