7501da93eadaf446e82d89ee1e187de76f5789fa
zahrayi <134525899+zahrayi@users.noreply.github.com>
2024-01-04 23:39:03 +0330

    add

--- minheap.cpp
+++ minheap.cpp
@@ -25,4 +25,19 @@ void heapify(int arr[], int n, int i)
         heapify(arr, n, smallest);
     }
 }
+void deleteRoot(int arr[], int& n)
+{
+    int lastElement = arr[n - 1];
+    arr[0] = lastElement;
+    n = n - 1;
+    heapify(arr, n, 0);
+}
+
+void buildMinHeap(int arr[], int n)
+{
+    for (int i = (n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+}
+
 
