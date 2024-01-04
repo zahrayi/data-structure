09f9045a23f00a9428a4a3f28393e96484883d06
elinaaslanian <149394302+elinaaslanian@users.noreply.github.com>
2024-01-04 22:20:04 +0330

    change

---
+++ countingsortt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class counting{
+
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
