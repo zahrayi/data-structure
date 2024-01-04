a10d0f0d6b40ddda53d50e44b03fa40dbdc2ec0f
zahrayi <134525899+zahrayi@users.noreply.github.com>
2024-01-04 23:22:01 +0330

    update

--- countingsortt.cpp
+++ countingsortt.cpp
@@ -12,4 +12,35 @@ int findMax(int arr[], int n) {
     }
     return max;
 }
+void countingSort(int arr[], int n) {
+    int max = findMax(arr, n);
+    int* count = new int[max + 1](); 
+
+    
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    
+    int* output = new int[n];
+
+    
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    delete[] count;
+    delete[] output;
+};
 
