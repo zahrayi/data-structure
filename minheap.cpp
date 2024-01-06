f4fa5c2681436fbe211efbfbdb65b30977513677
zahrayi <134525899+zahrayi@users.noreply.github.com>
2024-01-07 00:07:59 +0330

    Update minheap.cpp

--- minheap.cpp
+++ minheap.cpp
@@ -25,6 +25,18 @@ void heapify(int arr[], int n, int i)
         heapify(arr, n, smallest);
     }
 }
+void insert(int arr[], int& n, int key)
+{
+    n = n + 1;
+    int i = n - 1;
+    arr[i] = key;
+
+    while (i != 0 && arr[(i - 1) / 2] > arr[i]) {
+        swap(&arr[i], &arr[(i - 1) / 2]);
+        i = (i - 1) / 2;
+    }
+}
+
 void deleteRoot(int arr[], int& n)
 {
     int lastElement = arr[n - 1];
